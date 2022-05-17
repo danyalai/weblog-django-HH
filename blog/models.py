67a5d7bb6f69aac4139acb74cd792d16471dab58
aryan <aryantahami@gmail.com>
2022-05-17 19:16:44 +0430

    views data from db

--- blog/models.py
+++ blog/models.py
@@ -8,11 +8,10 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=100)
     text = models.TextField()
-    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
-    status = models.CharField(choices=STATUS_CHOICES,max_length=3)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=3)
 
     def __str__(self):
         return self.title
-
