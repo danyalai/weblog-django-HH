8d8e27f9d56acd6462b7ffc4ab8b6bdd346d05ac
aryan <aryantahami@gmail.com>
2022-05-18 20:57:14 +0430

    use dynamic url

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Post
@@ -6,3 +7,8 @@ from .models import Post
 def post_list_views(request):
     posts_list = Post.objects.all()
     return render(request, 'blog/posts_list.html', {'posts_list': posts_list})
+
+
+def post_detail_view(request, pk):
+    print('ID IN URL', pk)
+    return HttpResponse(f'Id:{pk}')
