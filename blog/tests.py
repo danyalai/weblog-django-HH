1bd52b00292ae9907ca1fda751cf6c0c7b131873
aryan <aryantahami@gmail.com>
2022-05-22 20:22:54 +0430

    test in django

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.models import User
+from django.shortcuts import reverse
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+
+class BlogPostTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='user1')
+        self.post1 = Post.objects.create(
+            title='post1',
+            text='this is test description',
+            status=Post.STATUS_CHOICES[0],
+            author=self.user,
+        )
+
+    def test_post_list_url(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_list_url_by_name(self):
+        response = self.client.get(reverse('posts_list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_title_on_blog_list_page(self):
+        response = self.client.get(reverse('posts_list'))
+        # self.assertContains(response, 'post1')
+        self.assertContains(response, self.post1.title)
+
+    def test_post_details_url(self):
+        response = self.client.get(f'/blog/{self.post1.id}/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_details_url_by_name(self):
+        response = self.client.get(reverse('post_detail', args=[self.post1.id]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_details_on_blog_detail_page(self):
+        response = self.client.get(reverse('post_detail', args=[self.post1.id]))
+        self.assertContains(response, self.post1.title)
+        self.assertContains(response, self.post1.text)
+
+    def test_status_404_if_post_id_not_exist(self):
+        response = self.client.get(reverse('post_detail', args=[999]))
+        self.assertEqual(response.status_code, 404)
