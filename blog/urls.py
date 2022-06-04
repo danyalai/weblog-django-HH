5407f6d1f1762d36226d95a6f0fff6b3247f2a4e
aryan <aryantahami@gmail.com>
2022-06-04 19:55:40 +0430

    complete "forms" in vscode

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.post_list_views, name='posts_list'),
-    path('<int:pk>/', views.post_detail_view, name='post_detail')
+    path('<int:pk>/', views.post_detail_view, name='post_detail'),
+    path('form', views.post_form, name='post_form')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
+from turtle import title
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 from .models import Post
+from .forms import NewPostForm
 
 
 def post_list_views(request):
@@ -11,3 +14,27 @@ def post_list_views(request):
 def post_detail_view(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_form(request):
+    if request.method == 'POST':
+        form = NewPostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = NewPostForm()
+    else:
+        form = NewPostForm()
+    return render(request, 'blog/add_post.html', context={'form':form})    
+
+
+
+    # if request.method == 'POST':
+    #     post_title = request.POST.get('title')
+    #     post_text = request.POST.get('text')
+    #     user = User.objects.all()[0]
+    #     Post.objects.create(title=post_title,text=post_text, author=user ,status='pub')
+    #     # print(request.POST.get('title'))   
+    #     # print(request.POST.get('text'))
+    # else:
+    #     print('GET request')    
+    # return render(request, 'blog/add_post.html')
